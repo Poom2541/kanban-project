62fb223625cce8d96b857b2b7da32303e3f39db7
Poom <poomfilm2541@gmail.com>
2020-12-03 16:57:42 +0700

    เพิ่ม drop

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,59 @@
 <template>
   <div id="app">
-    <Kanban :data="data"/>
+    <Kanban :data="data" :create_task_submit="create_task_submit"/>
   </div>
 </template>
 
 <script>
-import Kanban from "./components/Kanban";
+import Kanban from "./components/Kanban"
 
 export default {
-  name: "App",
+  name: 'App',
   components: {
-    Kanban,
+    Kanban
   },
-  data() {
-    return {
-      data: [
+  data(){
+    return{
+      data:[
         {
-          name: "todo",
-          color: "#FF99FF",
-          tasks: [],
+          name:"todo",
+          color:"#FA8072",
+          tasks:[{
+            task_name:"test",
+            },
+          ],
         },
         {
-          name: "doing",
-          color: "#33FF99",
-          tasks: [],
+          name:"doing",
+          color:"#6495ED",
+          tasks:[{
+            task_name:"test",
+            },
+          ],
         },
         {
-          name: "done",
-          color: "#3366FF",
-          tasks: [],
-        },
-      ],
-    };
+          name:"done",
+          color:"#FFA07A",
+          tasks:[{
+            task_name:"test",
+            },
+          ],
+        }
+      ]
+    }
   },
-};
+  methods:{
+    create_task_submit(column_index,item){
+      this.data[column_index].tasks.push(item)
+
+    }
+  }
+}
 </script>
 
 <style>
 #app {
   height: 100vh;
+
 }
 </style>
