e893b0aa3c6c24f6a33d35a168339f4b6bd441ec
Poom <poomfilm2541@gmail.com>
2020-12-03 14:02:50 +0700

    แก้ไข

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,39 @@
 <template>
   <div id="app">
-    <Kanban/>
-    
-    
+    <Kanban :data="data"/>
   </div>
 </template>
 
 <script>
-import Kanban from "./components/Kanban"
+import Kanban from "./components/Kanban";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    Kanban
-    
-  }
-}
+    Kanban,
+  },
+  data() {
+    return {
+      data: [
+        {
+          name: "todo",
+          color: "#FF99FF",
+          tasks: [],
+        },
+        {
+          name: "doing",
+          color: "#33FF99",
+          tasks: [],
+        },
+        {
+          name: "done",
+          color: "#3366FF",
+          tasks: [],
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
